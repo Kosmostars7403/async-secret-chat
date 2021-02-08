4fa3e123073b83b49b5464c62f369b27c81e8453
Kosmostars7403 <3699818@mail.ru>
2021-02-08 16:55:55 +0300

    Add CLI options

--- main.py
+++ main.py
@@ -2,20 +2,31 @@ import asyncio
 from datetime import datetime
 
 import aiofiles
+import configargparse
 
 
-async def connect_to_chat(message):
+async def connect_to_chat(options):
     reader, writer = await asyncio.open_connection(
-        'minechat.dvmn.org', 5000)
+        options.host, options.port)
 
     while True:
         chat_message = await reader.readline()
         message_received_time = datetime.now().strftime('%d.%m.%y %H:%M:%S')
         chat_message_log = f'[{message_received_time}] {chat_message.decode()}'
         print(chat_message_log)
-        async with aiofiles.open('chat_log.txt', mode='a', encoding='utf-8') as file:
+        async with aiofiles.open(options.history, mode='a', encoding='utf-8') as file:
             await file.write(chat_message_log)
 
 
+def get_application_options():
+    parser = configargparse.ArgParser(default_config_files=['./config.txt'])
+    parser.add('--host', help='Host for connection.')
+    parser.add('--port', help='Port for connection.')
+    parser.add('--history', help='Path fo history file.')
+
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    asyncio.run(connect_to_chat('Hello World!'))
+    options = get_application_options()
+    asyncio.run(connect_to_chat(options))
